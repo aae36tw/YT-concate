20be1b2961886416c6308edafea8281705761262
Anderson <aae36tw@gmail.com>
2022-08-21 06:19:20 +0800

    Refactor code into pipeline design pattern

--- YT_concate/main.py
+++ YT_concate/main.py
@@ -1,43 +1,24 @@
-#API : AIzaSyC8F7aeo6gH-LkvuVJ4rRzo3Lg2C6y9iiE
-
-import urllib.request
-import json
-from settings import API_KEY
-print(API_KEY)
+from pipeline.Steps.get_video_list import GetVideoList
+from pipeline.Steps.step import StepException
+from pipeline.pipeline import Pipeline
+from pipeline.Steps.get_video_list import GetVideoList
 
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 
-def get_all_video_in_channel(channel_id):
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
 
-    # api_key = os.getenv('API_KEY') 將api_key存至環境變數(搜尋Edit the system...點選enviornment variable)
+    steps = [
+        GetVideoList(),
+    ]
 
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
-
-    first_url = base_search_url + \
-        'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(
-            API_KEY, channel_id)
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-#video_list = get_all_video_in_channel(CHANNEL_ID)
-
-# print(len(video_list))
+if __name__ == '__main__':
+    main()
