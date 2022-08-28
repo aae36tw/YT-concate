ca22ca37bf2c3079de508df3607118740c082783
Anderson <aae36tw@gmail.com>
2022-08-28 08:01:50 +0800

    Successfully download captions and save video list to fils

--- YT_concate/main.py
+++ YT_concate/main.py
@@ -1,7 +1,10 @@
-from pipeline.Steps.get_video_list import GetVideoList
-from pipeline.Steps.step import StepException
+from pipeline.Steps.download_captions import DownloadCaptions
 from pipeline.pipeline import Pipeline
+from pipeline.Steps.step import StepException
 from pipeline.Steps.get_video_list import GetVideoList
+from utils import Utils
+from pipeline.Steps.preflight import Preflight
+from pipeline.Steps.postflight import Postflight
 
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
@@ -13,11 +16,15 @@ def main():
     }
 
     steps = [
+        Preflight(),
         GetVideoList(),
+        DownloadCaptions(),
+        Postflight(),
     ]
 
+    utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs)
+    p.run(inputs, utils)
 
 
 if __name__ == '__main__':
