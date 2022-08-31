52e27a2cbc8c1b76c0014c2e6551dc98d3a15363
Anderson <aae36tw@gmail.com>
2022-09-01 07:15:46 +0800

    Finished downloading videos

--- YT_concate/main.py
+++ YT_concate/main.py
@@ -1,7 +1,10 @@
 from pipeline.Steps.preflight import Preflight
 from pipeline.Steps.get_video_list import GetVideoList
+from pipeline.Steps.initialize_yt import InitializeYT
 from pipeline.Steps.download_captions import DownloadCaptions
 from pipeline.Steps.read_caption import ReadCaption
+from pipeline.Steps.search import Search
+from pipeline.Steps.download_videos import DownloadVideos
 from pipeline.Steps.postflight import Postflight
 from pipeline.Steps.step import StepException
 from pipeline.pipeline import Pipeline
@@ -12,14 +15,18 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 def main():
     inputs = {
-        'channel_id': CHANNEL_ID
+        'channel_id': CHANNEL_ID,
+        'search_word': 'incredible',
     }
 
     steps = [
         Preflight(),
         GetVideoList(),
+        InitializeYT(),
         DownloadCaptions(),
         ReadCaption(),
+        Search(),
+        DownloadVideos(),
         Postflight(),
     ]
 
